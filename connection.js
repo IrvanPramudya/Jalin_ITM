62452abdcb62509a22c2dfb3a83fb05983cb22f6
Irvan Pramudya <irvan.liverpuddillan31@gmail.com>
2025-02-26 09:17:52 +0700

    initial commit

---
+++ connection.js
@@ -0,0 +1,64 @@
+const database = require('node-jt400');
+
+class databaseConnection{
+    constructor(configuration){
+        this.configuration = configuration
+        this.poolConnection = database.pool(configuration)
+    }
+    async getDateWithOffset(offset = 0) {
+    let date = new Date();
+    date.setDate(date.getDate() + offset);
+
+    let getYear = date.getFullYear().toString().substr(-2, 2);
+    let getMonth = (date.getMonth() + 1).toString().padStart(2, '0');
+    let getDate = date.getDate().toString().padStart(2, '0');
+
+    return `1${getYear}${getMonth}${getDate}`;
+    }   
+    async getConnection(){
+        try{
+            console.time('TRYING CONNECT DATABASE')
+            await database.connect(this.configuration)
+            console.timeEnd('TRYING CONNECT DATABASE')
+            console.info('DATABASE SUCCESFULLY CONNECTED')
+        }
+        catch(err){
+            console.error('Failed to get connection from database : ', err)
+            throw err
+        }
+    }
+    async executeQuery(query,params=[]){
+        try{
+            return await this.poolConnection.update(query,params)
+        }
+        catch(error){
+            console.error('Failed to execute query : ', error)
+            throw error
+        }
+    }
+    async executeTransactionalQuery(queries = []){
+        try{
+            for(const{query,params} of queries){
+                await this.executeQuery(query,params)
+                await this.closeConnection()
+            }
+        }
+        catch(error){
+            console.error(`TRANSACTION FAILED, ROLLING BACK : ${error}`)
+            throw error
+        }
+        finally{
+            await this.closeConnection()
+        }
+    }
+    async closeConnection() {
+        try {
+            await this.poolConnection.close();
+            console.log('Connection closed successfully');
+        } catch (error) {
+            console.error('Error closing connection:', error);
+        }
+    }
+}
+
+module.exports = databaseConnection
---
+++ updatebit15.js
@@ -0,0 +1,33 @@
+const connection = require('../ITM/connection');
+const itmConfiguration = {
+    host :'13.131.5.25',
+    user: '@irvan',
+    password:'jalin123',
+    connectionLimit:5,
+    keepAlive: false,
+}
+const connectiontoITMAS400 = new connection(itmConfiguration)
+async function updateNewDate(){
+    await connectiontoITMAS400.getConnection()
+    const dates = {
+        previousDate: await connectiontoITMAS400.getDateWithOffset(-1), // Tanggal kemarin
+        todayDate: await connectiontoITMAS400.getDateWithOffset(0),     // Tanggal hari ini
+        nextDate: await connectiontoITMAS400.getDateWithOffset(1)       // Tanggal besok
+    };
+    const queries = [
+        {query:`UPDATE ASIDJL46M1.zparct0p SET PCPSDT=?,PCCSDT = ?,PCNSDT=?`,params:[dates.previousDate,dates.todayDate,dates.nextDate]},
+        {query:`UPDATE ASIDJLN46M.zparct0p SET PCPSDT=?,PCCSDT = ?,PCNSDT=?`,params:[dates.previousDate,dates.todayDate,dates.nextDate]},
+        {query:`UPDATE ASIDJL46N2.zparct0p SET PCPSDT=?,PCCSDT = ?,PCNSDT=?`,params:[dates.previousDate,dates.todayDate,dates.nextDate]},
+        {query:`UPDATE ASIDJLN46N.zparct0p SET PCPSDT=?,PCCSDT = ?,PCNSDT=?`,params:[dates.previousDate,dates.todayDate,dates.nextDate]},
+        {query:`UPDATE ASID0902LK.zparct0p SET PCPSDT=?,PCCSDT = ?,PCNSDT=?`,params:[dates.previousDate,dates.todayDate,dates.nextDate]}
+    ]
+    await connectiontoITMAS400.executeTransactionalQuery(queries)
+}
+updateNewDate().then(()=>{
+    console.info("UPDATED DONE")
+}).catch(err =>{
+    console.error("ERROR UPDATING with ",err)
+}).finally(()=>{
+    connectiontoITMAS400.closeConnection()
+})
+module.exports=updateNewDate
